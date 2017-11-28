f2f0390b092dcd37c6a15073fb7b517d941d0093
3q12 <si8363@soongsil.ac.kr>
2017-11-29 02:04:20 +0900

    fix error

--- MrTraveler/MrTravelerView.cpp
+++ MrTraveler/MrTravelerView.cpp
@@ -1,4 +1,4 @@
-
+﻿
 // MrTravelerView.cpp : CMrTravelerView 클래스의 구현
 //
 
@@ -64,23 +64,23 @@ void CMrTravelerView::OnDraw(CDC* pDC)
 	CTime cTime = CTime::GetCurrentTime();
 	int curYear, curMonth, curDate;
 	curYear = cTime.GetYear();
-	curMonth= cTime.GetMonth();
+	curMonth = cTime.GetMonth();
 	curDate = cTime.GetDay();
 	int curDay = cTime.GetDayOfWeek();
 	int firstDay = curDay - (curDate % 7 - 1);//���� �� 1�� ����
 
 	CRgn monthRgn;
-	monthRgn.CreateEllipticRgn((1400 - 150) / 2+25, 50-50, (1400 - 150) / 2+ 150+25, 200-50);
+	monthRgn.CreateEllipticRgn((1400 - 150) / 2 + 25, 50 - 50, (1400 - 150) / 2 + 150 + 25, 200 - 50);
 	pDC->FillRgn(&monthRgn, &CBrush(RGB(154, 202, 235)));
-	
+
 	CFont font;
-	font.CreatePointFont(400, _T("����ü")); 
+	font.CreatePointFont(400, _T("����ü"));
 	CString strMonth;
 	strMonth.Format(_T("%d"), curMonth);
 	pDC->SetBkColor(RGB(154, 202, 235));
 	pDC->SetTextColor(RGB(255, 255, 255));
 	pDC->SelectObject(&font);
-	pDC->TextOut((1400 - 150) / 2 + 32+25, 50+32+5-50,strMonth);
+	pDC->TextOut((1400 - 150) / 2 + 32 + 25, 50 + 32 + 5 - 50, strMonth);
 
 	CRgn dayRgn[7];
 	for (int day = 0; day < 7; day++) {
@@ -88,12 +88,12 @@ void CMrTravelerView::OnDraw(CDC* pDC)
 		pDC->FillRgn(&dayRgn[day], &CBrush(RGB(154, 202, 235)));
 
 		CFont font;
-		font.CreatePointFont(150, _T("����ü"));
+		font.CreatePointFont(150, _T("바탕"));
 		pDC->SetBkColor(RGB(154, 202, 235));
 		pDC->SetTextColor(RGB(255, 255, 255));
 		pDC->SelectObject(&font);
 		if (day == 0)
-			pDC->TextOut(25 + day * 200+80, 210, _T("SUN"));
+			pDC->TextOut(25 + day * 200 + 80, 210, _T("SUN"));
 		else if (day == 1)
 			pDC->TextOut(25 + day * 200 + 80, 210, _T("MON"));
 		else if (day == 2)
@@ -101,10 +101,10 @@ void CMrTravelerView::OnDraw(CDC* pDC)
 		else if (day == 3) {
 			pDC->TextOut(25 + day * 200 + 80, 210, _T("WED"));
 			pDC->SetBkColor(RGB(255, 255, 255));
-			pDC->SetTextColor(RGB(0,0,0));
+			pDC->SetTextColor(RGB(0, 0, 0));
 			CString strYear;
 			strYear.Format(_T("%d"), curYear);
-			pDC->TextOut(15+ day * 200 + 80, 160, strYear);
+			pDC->TextOut(15 + day * 200 + 80, 160, strYear);
 		}
 		else if (day == 4)
 			pDC->TextOut(25 + day * 200 + 80, 210, _T("THU"));
@@ -117,26 +117,26 @@ void CMrTravelerView::OnDraw(CDC* pDC)
 
 	int end_of_mon[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
 	if (!(curYear % 4) && ((curYear % 100) || !(curYear % 400))) end_of_mon[1] = 29;
-	
+
 	CRgn dateRgn[43];
-	for (int day = firstDay-1; day < end_of_mon[curMonth-1]+firstDay-1; day++) {
+	for (int day = firstDay - 1; day < end_of_mon[curMonth - 1] + firstDay - 1; day++) {
 		dateRgn[day].CreateRoundRectRgn((day) % 7 * 200 + 25,
-			250+(day)/7*100,
-			((day) % 7 + 1) * 200 + 25, 
-			250 + ((day) / 7+1) * 100,
+			250 + (day) / 7 * 100,
+			((day) % 7 + 1) * 200 + 25,
+			250 + ((day) / 7 + 1) * 100,
 			20, 20);
 		pDC->FillRgn(&dateRgn[day], &CBrush(RGB(255, 255, 255)));
-		pDC->FrameRgn(&dateRgn[day], &CBrush(RGB(216,216,216)), 2, 2);
+		pDC->FrameRgn(&dateRgn[day], &CBrush(RGB(216, 216, 216)), 2, 2);
 
 		CFont font;
-		font.CreatePointFont(100, _T("���"));
+		font.CreatePointFont(100, _T("바탕"));
 		CString strDate;
-		strDate.Format(_T("%d"), day-firstDay+2);
+		strDate.Format(_T("%d"), day - firstDay + 2);
 		pDC->SetBkColor(RGB(255, 255, 255));
 		pDC->SetTextColor(RGB(0, 0, 0));
 		pDC->SelectObject(&font);
-		pDC->TextOut((day) % 7 * 200 + 25+10,
-			250 + (day) / 7 * 100+10, strDate);
+		pDC->TextOut((day) % 7 * 200 + 25 + 10,
+			250 + (day) / 7 * 100 + 10, strDate);
 	}
 
 	CRgn tapRgn[6];
@@ -145,24 +145,24 @@ void CMrTravelerView::OnDraw(CDC* pDC)
 		pDC->FillRgn(&tapRgn[i], &CBrush(RGB(154, 202, 235)));
 
 		CFont font;
-		font.CreatePointFont(100, _T("����ü"));
+		font.CreatePointFont(100, _T("바탕"));
 		pDC->SetBkColor(RGB(154, 202, 235));
 		pDC->SetTextColor(RGB(255, 255, 255));
 		pDC->SelectObject(&font);
 		if (i == 0)
-			pDC->TextOut(25 + i * 200 + 40, 860, _T("Ķ����"));
+			pDC->TextOut(25 + i * 200 + 40, 860, _T("캘린더"));
 		else if (i == 1)
-			pDC->TextOut(25 + i * 200 + 50, 860, _T("�� ��"));
+			pDC->TextOut(25 + i * 200 + 50, 860, _T("일정"));
 		else if (i == 2)
 			pDC->TextOut(25 + i * 200 + 30, 860, _T("TO DO List"));
 		else if (i == 3)
-			pDC->TextOut(25 + i * 200 + 40, 860, _T("�����"));
+			pDC->TextOut(25 + i * 200 + 40, 860, _T("가계부"));
 		else if (i == 4)
-			pDC->TextOut(25 + i * 200 + 35, 860, _T("���̾"));
+			pDC->TextOut(25 + i * 200 + 35, 860, _T("다이어리"));
 		else if (i == 5)
 			pDC->TextOut(25 + i * 200 + 45, 860, _T("INFO"));
 	}
-
+}
 
 BOOL CMrTravelerView::OnPreparePrinting(CPrintInfo* pInfo)
 {
